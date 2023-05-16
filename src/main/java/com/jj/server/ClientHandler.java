df152dfb81e789bf273b29defc8214f58db60aa8
ButterflyLion <orosztama@gmail.com>
2023-05-16 23:16:02 +0100

    modified client and created clientHandler

---
+++ src/main/java/com/jj/server/ClientHandler.java
@@ -0,0 +1,86 @@
+package com.jj.server;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.util.ArrayList;
+
+// https://www.youtube.com/watch?v=gLfuZrrfKes&t=359s
+public class ClientHandler implements Runnable{
+
+    public static ArrayList<ClientHandler> clientHandlers = new ArrayList<>();
+    private Socket socket;
+    private BufferedReader bufferedReader;
+    private BufferedWriter bufferedWriter;
+    private String clientUsername;
+
+    public ClientHandler(Socket socket){
+        try{
+            this.socket = socket;
+            // what we use to send
+            this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            // what the client is sending
+            this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            this.clientUsername = bufferedReader.readLine();
+            clientHandlers.add(this);
+            broadcastMessage("SERVER: " + clientUsername+ " has entered the chat");
+        } catch(IOException io){
+            closeEverything(socket, bufferedReader, bufferedWriter);
+        }
+    }
+
+    @Override
+    public void run() {
+        // listen to separate messages
+        String input;
+        while(socket.isConnected()){
+            try{
+                input = bufferedReader.readLine();
+                broadcastMessage(input);
+            } catch(IOException io){
+                closeEverything(socket, bufferedReader, bufferedWriter);
+                break;
+            }
+        }
+    }
+
+    public void broadcastMessage(String message){
+        for(ClientHandler clientHandler : clientHandlers){
+            try{
+                if(!clientHandler.clientUsername.equals(clientUsername)){
+                    clientHandler.bufferedWriter.write(message);
+                    clientHandler.bufferedWriter.newLine();
+                    clientHandler.bufferedWriter.flush();
+                }
+            } catch(IOException io){
+                closeEverything(socket, bufferedReader, bufferedWriter);
+            }
+        }
+    }
+
+    public void removeClientHandler(){
+        clientHandlers.remove(this);
+        broadcastMessage("SERVER: "+clientUsername+" has left the chat");
+    }
+
+    public void closeEverything(Socket socket, BufferedReader bufferedReader, BufferedWriter bufferedWriter){
+        removeClientHandler();
+        try{
+            if(bufferedReader != null){
+                bufferedReader.close();
+            }
+            if(bufferedWriter != null){
+                bufferedWriter.close();
+            }
+            if(socket != null){
+                socket.close();
+            }
+        } catch(IOException io){
+            io.printStackTrace();
+        }
+    }
+    
+}
