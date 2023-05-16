0035ff8cf40f48b6bc9bdb982155586866153aad
galileo-dev <fionn2200@gmail.com>
2023-05-16 15:43:03 +0100

    testing

--- README.md
+++ README.md
@@ -25,3 +25,11 @@ the executable `.jar` file will be in the `target/` directory
 ```
 mvn test
 ```
+
+# Sources
+
+- [BufferedReader](https://docs.oracle.com/javase/8/docs/api/java/io/BufferedReader.html)
+- [Sockets](https://docs.oracle.com/javase/8/docs/api/java/net/Socket.html)
+- [Testing with JUnit](https://junit.org/junit5/docs/current/user-guide/#overview-getting-started)
+- [Testing with main method](https://stackoverflow.com/questions/36349827/testing-main-method-by-junit)
+- [Threading](https://www.w3schools.com/java/java_threads.asp)
--- src/test/java/com/jj/server/ServerTest.java
+++ src/test/java/com/jj/server/ServerTest.java
@@ -2,17 +2,48 @@ package com.jj.server;
 
 import static org.junit.Assert.assertTrue;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import java.net.*;
 
 /**
  * Unit test for simple App.
  */
+
+class ServerThread extends Thread {
+    public void run() {
+        String[] args = {};
+        Server.main(args);
+    }
+}
+
 public class ServerTest {
     /**
      * Rigorous Test :-)
      */
+
+    Thread serverThread;
+    Socket clientSocket;
+
+    @Before
+    public void setUp() throws Exception {
+        // start the server
+        serverThread = new ServerThread();
+        serverThread.start();
+        // connect to the server
+        Thread.sleep(1000);
+        clientSocket = new Socket("localhost", 8080);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+        serverThread.interrupt();
+    }
+
     @Test
-    public void shouldAnswerWithTrue() {
+    public void shouldServer() {
         assertTrue(true);
     }
 }
