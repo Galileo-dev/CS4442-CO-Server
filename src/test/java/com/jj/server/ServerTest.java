5976a129301317978f4135dba074f64a2169f16a
galileo-dev <fionn2200@gmail.com>
2023-05-18 15:34:31 +0100

    conversation test

--- src/test/java/com/jj/server/ServerTest.java
+++ src/test/java/com/jj/server/ServerTest.java
@@ -2,9 +2,7 @@ package com.jj.server;
 
 import static org.junit.Assert.assertTrue;
 
-import org.junit.After;
 import org.junit.AfterClass;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -108,6 +106,49 @@ public class ServerTest {
         assertTrue(listMessage.equals("1 users online"));
     }
 
+    @Test
+    public void serverConverse() throws UnknownHostException, IOException {
+        // create a new client first
+        Socket clientSocket2 = new Socket("localhost", 8080);
+        // give them a namme
+        BufferedWriter bufferedWriter2 = new BufferedWriter(new OutputStreamWriter(clientSocket2.getOutputStream()));
+        BufferedReader bufferedReader2 = new BufferedReader(new InputStreamReader(clientSocket2.getInputStream()));
+        // clear the reader
+
+        bufferedWriter2.write("username2\n");
+        bufferedWriter2.flush();
+
+        // client 1 reads the user join message
+        String userJoinMessage = bufferedReader.readLine();
+        assertTrue(userJoinMessage.equals("SERVER: username2 has entered the chat"));
+
+        // read welcome message
+        String welcomeMessage = bufferedReader2.readLine();
+        System.out.println(welcomeMessage);
+        assertTrue(welcomeMessage.equals("Welcome username2!"));
+
+        // now let's get them to talk
+        bufferedWriter.write("hello\n");
+        bufferedWriter.flush();
+
+        // client 2 reads the message
+        String message1 = bufferedReader2.readLine();
+        System.out.println(message1);
+        assertTrue(message1.equals("username: hello"));
+
+        // client 2 responds
+        bufferedWriter2.write("hi\n");
+        bufferedWriter2.flush();
+
+        String message2 = bufferedReader.readLine();
+        assertTrue(message2.equals("username2: hi"));
+
+        bufferedReader2.close();
+        bufferedWriter2.close();
+        clientSocket2.close();
+    }
+
+    @Test
     public void serverExitCommand() throws UnknownHostException,
             IOException {
         // send username
