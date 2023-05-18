fc68c6b9b5ea8a14af09686c793e2cfa356356a5
galileo-dev <fionn2200@gmail.com>
2023-05-18 11:20:13 +0100

    Updated tests

--- src/test/java/com/jj/server/ServerTest.java
+++ src/test/java/com/jj/server/ServerTest.java
@@ -3,8 +3,16 @@ package com.jj.server;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.*;
 
 /**
@@ -13,8 +21,13 @@ import java.net.*;
 
 class ServerThread extends Thread {
     public void run() {
-        String[] args = {};
-        Server.main(args);
+        try {
+            String[] args = {};
+            Server.main(args);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
 
@@ -23,27 +36,74 @@ public class ServerTest {
      * Rigorous Test :-)
      */
 
-    Thread serverThread;
-    Socket clientSocket;
+    private static Thread serverThread;
 
-    @Before
-    public void setUp() throws Exception {
+    @BeforeClass
+    public static void setUp() throws Exception {
         // start the server
         serverThread = new ServerThread();
         serverThread.start();
-        // connect to the server
-        Thread.sleep(1000);
-        clientSocket = new Socket("localhost", 8080);
+
+        // check that the server is running before we can run tests
+        int maxAttempts = 10;
+        int attempts = 0;
+        while (attempts < maxAttempts) {
+            try {
+                Socket clientSocket = new Socket("localhost", 8080);
+                clientSocket.close();
+                break;
+            } catch (ConnectException e) {
+                attempts++;
+                Thread.sleep(100);
+            }
+        }
+        if (attempts == maxAttempts) {
+            throw new Exception("Server failed to start");
+        }
+
     }
 
-    @After
-    public void tearDown() throws Exception {
-
+    @AfterClass
+    public static void tearDown() throws Exception {
         serverThread.interrupt();
     }
 
     @Test
-    public void shouldServer() {
-        assertTrue(true);
+    public void serverShouldAllowConnection() throws UnknownHostException,
+            IOException {
+        Socket clientSocket = new Socket("localhost", 8080);
+        assertTrue(clientSocket.isConnected());
+        clientSocket.close();
+    }
+
+    @Test
+    public void serverShouldAllowMultipleConnections() throws UnknownHostException, IOException {
+        Socket clientSocket1 = new Socket("localhost", 8080);
+        assertTrue(clientSocket1.isConnected());
+        Socket clientSocket2 = new Socket("localhost", 8080);
+        assertTrue(clientSocket2.isConnected());
+        clientSocket1.close();
+        clientSocket2.close();
+    }
+
+    @Test
+    public void serverShouldGiveStartMessage() throws UnknownHostException,
+            IOException {
+        Socket clientSocket = new Socket("localhost", 8080);
+        assertTrue(clientSocket.isConnected());
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+
+        // send username
+        bufferedWriter.write("username\n");
+        bufferedWriter.flush();
+
+        // read start message
+        String startMessage = bufferedReader.readLine();
+
+        assertTrue(startMessage.equals("Welcome username!"));
+        bufferedReader.close();
+        bufferedWriter.close();
+        clientSocket.close();
     }
 }
